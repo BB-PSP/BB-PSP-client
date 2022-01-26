97585a2d104b9159b257ccaef9bf4b2633ee97ff
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-27 00:20:45 +0900

    반응형 웹 테스트중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { theme } from '../styles/theme';
 
 const TestDiv = styled.div`
-  width: 200px;
-  background: yellow;
+  @media ${(props) => props.theme.tablet} {
+    width: 100%;
+    height: 300px;
+    margin: 0 auto;
+    background: #333;
+  }
+  @media ${(props) => props.theme.mobile} {
+    width: 100%;
+    height: 300px;
+    margin: 0 auto;
+    background: #123456;
+  }
 `;
 
 const Home: NextPage = () => {
-  return <TestDiv>Home</TestDiv>;
+  return <TestDiv></TestDiv>;
 };
 
 export default Home;
