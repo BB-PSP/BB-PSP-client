cdddb651e723d68524991d6a7e6b731dcb36fc1b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-27 00:18:29 +0900

    미디어 쿼리용 종단점 설정

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,8 @@
+// airbnb css-in-javascript naming guide
+const breakpoints = {
+  small: '@media (max-width: 639px)',
+  medium: '@media (max-width: 1047px)',
+  large: '@media (min-width: 1048px)',
+};
+
+export { breakpoints };
