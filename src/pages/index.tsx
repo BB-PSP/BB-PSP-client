27bbfae61b7781bda3337251ce5e0d7fadf8664c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-26 17:19:24 +0900

    stlyed reset 설치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const TestDiv = styled.div`
+  width: 200px;
+  background: yellow;
+`;
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return <TestDiv>Home</TestDiv>;
 };
 
 export default Home;
