b93c94c4e0ca66ed5e122bfe74a44f3cc4b748f4
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-04 14:16:07 +0900

    feat: :zap: add layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import GlobalStyle from '../styles/GlobalStyle';
-import { ThemeProvider } from 'styled-components';
-import GlobalFont from '../styles/GlobalFont';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyle />
-      <GlobalFont />
-      {/* <ThemeProvider theme={breakpoints}> */}
-      <Component {...pageProps} />
-      {/* </ThemeProvider> */}
-    </>
-  );
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
-export default MyApp;
