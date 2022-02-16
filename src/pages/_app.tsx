9af1bf8abc0b5e42f16f675144cc19fa303d31f2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-16 23:04:52 +0900

    style: update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,22 @@
-import type { ReactElement, ReactNode } from 'react';
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
+import React from 'react';
+import Layout from '../components/layout/Layout';
+import GlobalFont from '../styles/GlobalFont';
+import GlobalStyle from '../styles/GlobalStyle';
 
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
+type AppProps = {
+  Component: React.ElementType;
 };
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(<Component {...pageProps} />);
+function App({ Component }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <GlobalFont />
+      <Layout>
+        <Component />
+      </Layout>
+    </>
+  );
 }
+
+export default App;
