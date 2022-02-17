c0c0d0659657fe3154e9b0076042cfb8db7036f1
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-17 20:32:25 +0900

    feat: :art: update kakao browser

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import { breakpoints } from './media';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -13,7 +12,6 @@ const GlobalStyle = createGlobalStyle`
   body{
     background-color: #ccc;
     overflow-x: hidden;
-    
     font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   }
   a {
