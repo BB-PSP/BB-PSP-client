b47bd58c779bdbf4610b443cedc78c76eb7ad80d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-17 16:33:30 +0900

    test

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -13,6 +13,10 @@ const GlobalStyle = createGlobalStyle`
   body{
     background-color: #ccc;
     overflow-x: hidden;
+    ${breakpoints.small} {
+      height: 100%;
+      overflow-y: hidden;
+    }
     font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   }
   a {
