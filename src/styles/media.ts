d2af06c7da3417591bc19c95fd4e5f07c6ecf3b2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-22 16:19:22 +0900

    fix: :bug: fix media query

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -1,7 +1,7 @@
 // airbnb css-in-javascript naming guide
 const breakpoints = {
   small: '@media (max-width: 639px)',
-  medium: '@media (max-width: 1279px)',
+  medium: '@media (max-width: 1279px) and (min-width: 640px)',
   large: '@media (min-width: 1280px)',
 };
 
