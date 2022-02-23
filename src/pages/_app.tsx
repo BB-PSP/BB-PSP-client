75bde180110c079455a791598fbe4728226de3c8
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-23 21:42:55 +0900

    data mocking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
+import initMockAPI from '../mocks';
 import GlobalFont from '../styles/GlobalFont';
 import GlobalStyle from '../styles/GlobalStyle';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+if (process.env.NODE_ENV === 'development') {
+  initMockAPI();
+}
 
 type AppProps = {
   Component: React.ElementType;
 };
 
 function App({ Component }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <GlobalFont />
       <Layout>
         <Component />
       </Layout>
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
