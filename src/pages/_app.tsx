326f0fde251398baa6dbd233608914e7fba30d2f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-24 14:52:13 +0900

    perf: Hydrate Code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import { RecoilRoot } from 'recoil';
 import Layout from '../components/layout/Layout';
 import initMockAPI from '../mocks';
 import GlobalFont from '../styles/GlobalFont';
 import GlobalStyle from '../styles/GlobalStyle';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import type { AppProps } from 'next/app';
 
 if (process.env.NODE_ENV === 'development') {
   initMockAPI();
 }
 
-type AppProps = {
-  Component: React.ElementType;
-};
+// type AppProps = {
+//   Component: React.ElementType;
+// };
 
-function App({ Component }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <GlobalFont />
-      <Layout>
-        <Component />
-      </Layout>
+      <Hydrate state={pageProps.dehydratedState}>
+        <GlobalStyle />
+        <GlobalFont />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
