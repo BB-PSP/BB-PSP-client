4c57869c33019368ef1d02e7bd9951405d24deaa
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-28 16:54:05 +0900

    feat: :sparkles: create PlayerCard

---
+++ src/components/custom/PlayerCard.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 29rem;
+  height: 16.2rem;
+  display: flex;
+  flex-direction: row;
+  border-radius: 0.4rem;
+  box-shadow: 0.2rem 0.4rem 0.8rem rgba(0, 0, 0.25);
+`;
+
+const PhotoBox = styled.div`
+  width: 50%;
+  height: 100%;
+  border-top-left-radius: 0.4rem;
+  border-bottom-left-radius: 0.4rem;
+  background-color: white;
+`;
+
+const ContentsBox = styled.div`
+  width: 50%;
+  height: 100%;
+  border-top-right-radius: 0.4rem;
+  border-bottom-right-radius: 0.4rem;
+  background-color: rgba(39, 39, 39, 0.87);
+`;
+
+const LogoBox = styled.div`
+  width: 100%;
+  height: 8.8rem;
+  border-top-right-radius: 0.4rem;
+  border-bottom-right-radius: 0.4rem;
+  background-image: url('/image/teamLogo/kiwoom.png');
+  background-size: auto 3.7rem;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const ProfileBox = styled.div`
+  width: 100%;
+  height: 7.4rem;
+  border-top-right-radius: 0.4rem;
+  border-bottom-right-radius: 0.4rem;
+  padding: 0 2.2rem 2.3rem 2.2rem;
+  text-align: center;
+`;
+
+const Name = styled.h2`
+  font-family: 'RobotoMonoRegular';
+  font-size: 1.4rem;
+  line-height: 1.8rem;
+  color: #fff;
+`;
+
+const SmallBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 1.7rem;
+`;
+
+const Position = styled.h3`
+  font-family: 'RobotoMonoRegular';
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+  color: #fff;
+`;
+
+const Birthday = styled.h3`
+  font-family: 'RobotoMonoRegular';
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+  color: #fff;
+`;
+
+export default function PlayerCard() {
+  return (
+    <Wrapper>
+      <PhotoBox />
+      <ContentsBox>
+        <LogoBox />
+        <ProfileBox>
+          <Name>LEE.JEONG HU</Name>
+          <SmallBox>
+            <Position>CF</Position>
+            <Birthday>98.08.20</Birthday>
+          </SmallBox>
+        </ProfileBox>
+      </ContentsBox>
+    </Wrapper>
+  );
+}
