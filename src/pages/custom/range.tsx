87473d66e12434d46eb81078bd9e4afff03c6b1a
syoung125 <laura120555@gmail.com>
2022-03-03 21:29:18 +0900

    feat: :sparkles: add SalaryRangeSlider, AgeRangeSlider to custom/range page

--- src/pages/custom/range.tsx
+++ src/pages/custom/range.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
 import { breakpoints } from '../../styles/media';
+
+import AgeRangeSlider from '../../components/custom/RangeSlider/AgeRangeSlider';
+import _SalaryRangeSlider from '../../components/custom/RangeSlider/SalaryRangeSlider';
 
 const Wrapper = styled.div`
   display: flex;
@@ -49,11 +53,33 @@ const ContentsContainer = styled.div`
   flex-direction: column;
 `;
 
+const SalaryRangeSlider = styled(_SalaryRangeSlider)`
+  margin-bottom: 6rem;
+`;
+
 export default function Range() {
+  const [salaryRange, setSalaryRange] = useState<number[]>([30, 70]);
+  const [ageRange, setAgeRange] = useState<number[]>([25, 35]);
+
   return (
     <Wrapper>
       <SubTitle>BB:PSP(Baseball: Player Stats Prediction)</SubTitle>
-      <ContentsContainer></ContentsContainer>
+      <ContentsContainer>
+        <SalaryRangeSlider
+          label="salary"
+          value={salaryRange}
+          onChange={setSalaryRange}
+          min={0}
+          max={100}
+        />
+        <AgeRangeSlider
+          label="age"
+          value={ageRange}
+          onChange={setAgeRange}
+          min={20}
+          max={50}
+        />
+      </ContentsContainer>
     </Wrapper>
   );
 }
