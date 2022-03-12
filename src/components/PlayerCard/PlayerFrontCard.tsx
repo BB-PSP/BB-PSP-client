1bb9bf4a3d776241918f5fc2738104a0cd1e0647
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-12 23:22:00 +0900

    style: 디자인 수정

--- src/components/PlayerCard/PlayerFrontCard.tsx
+++ src/components/PlayerCard/PlayerFrontCard.tsx
@@ -26,83 +26,82 @@ const Wrapper = styled(motion.div)`
   }
 `;
 
-const PhotoBox = styled.div`
-  width: 100%;
-  height: 100%;
-  border-top-left-radius: 0.4rem;
-  border-bottom-left-radius: 0.4rem;
-  background-color: white;
-`;
-
 const ContentsBox = styled.div`
+  display: flex;
+  flex-direction: row;
   width: 100%;
   height: 100%;
-  border-top-right-radius: 0.4rem;
-  border-bottom-right-radius: 0.4rem;
+  border-radius: 0.4rem;
   background-color: rgba(39, 39, 39, 0.87);
 `;
 
-const LogoBox = styled.div`
-  width: 100%;
-  height: 8.8rem;
-  border-top-right-radius: 0.4rem;
-  border-bottom-right-radius: 0.4rem;
-  background-image: url('/image/teamLogo/kiwoom.png');
-  background-size: auto 3.7rem;
-  background-repeat: no-repeat;
-  background-position: center;
+const LeftBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-right: 1px solid #fff;
+  ${breakpoints.large} {
+    width: 15.9rem;
+    height: 9rem;
+    margin-top: 3.6rem;
+    padding-left: 2.9rem;
+  }
+  ${breakpoints.medium} {
+    width: 15.9rem;
+    height: 9rem;
+    margin-top: 3.6rem;
+    padding-left: 2.9rem;
+  }
 `;
 
-const ProfileBox = styled.div`
-  width: 100%;
-  height: 7.4rem;
-  border-top-right-radius: 0.4rem;
-  border-bottom-right-radius: 0.4rem;
-  padding: 0 2.2rem 2.3rem 2.2rem;
-  text-align: center;
-`;
-
-const Name = styled.h2`
-  font-family: 'RobotoMonoRegular';
+const Name = styled.h3`
+  font-family: 'RobotoMonoBold';
   font-size: 1.4rem;
-  line-height: 1.8rem;
+  line-height: 1.846rem;
   color: #fff;
 `;
 
-const SmallBox = styled.div`
+const LittleBox = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  padding-top: 1.7rem;
 `;
 
 const Position = styled.h3`
-  font-family: 'RobotoMonoRegular';
-  font-size: 1.2rem;
-  line-height: 1.6rem;
+  font-family: 'RobotoMonoBold';
+  font-size: 1.4rem;
+  line-height: 1.846rem;
   color: #fff;
+  padding-right: 1.6rem;
 `;
 
 const Birthday = styled.h3`
-  font-family: 'RobotoMonoRegular';
-  font-size: 1.2rem;
-  line-height: 1.6rem;
+  font-family: 'RobotoMonoBold';
+  font-size: 1.4rem;
+  line-height: 1.846rem;
   color: #fff;
+`;
+
+const RightBox = styled.div`
+  width: 13.1rem;
+  height: 100%;
+  background-image: url('/image/teamLogo/kiwoom.png');
+  background-size: auto 5.2rem;
+  background-repeat: no-repeat;
+  background-position: center;
 `;
 
 export default function PlayerFrontCard() {
   return (
     <Wrapper>
-      <PhotoBox />
       <ContentsBox>
-        <LogoBox />
-        <ProfileBox>
-          <Name>LEE.JEONG HU</Name>
-          <SmallBox>
+        <LeftBox>
+          <Name>이정후</Name>
+          <LittleBox>
             <Position>CF</Position>
             <Birthday>98.08.20</Birthday>
-          </SmallBox>
-        </ProfileBox>
+          </LittleBox>
+        </LeftBox>
+        <RightBox></RightBox>
       </ContentsBox>
     </Wrapper>
   );
