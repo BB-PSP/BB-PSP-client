86f36629f91500b3308b5db64f7518413b4949b7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-13 14:52:56 +0900

    feat: 모달창 구현

--- src/components/layout/ResultButton.tsx
+++ src/components/layout/ResultButton.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { breakpoints } from '../../styles/media';
+import PlayerCard from '../PlayerCard/PlayerCard';
+import PlayerFrontCard from '../PlayerCard/PlayerFrontCard';
 
 const Wrapper = styled.div`
   display: flex;
@@ -90,7 +93,86 @@ const ButtonText = styled.h3`
   color: #272727;
 `;
 
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CloseButton = styled.img`
+  width: 6.2rem;
+  height: 6.2rem;
+`;
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.05);
+  backdrop-filter: blur(5px);
+  animation: modal-bg-show 1s;
+  @keyframes modal-bg-show {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const ModalBlock = styled.div`
+  background-color: white;
+  border-radius: 1rem;
+  width: 76.6rem;
+  height: 80.8rem;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5.3rem;
+`;
+
+const Title = styled.h2`
+  font-family: 'RobotoMonoRegular';
+  font-size: 2.2rem;
+  line-height: 2.9rem;
+  color: #272727;
+  padding-bottom: 5.7rem;
+`;
+
+const Grid = styled.div`
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  height: 67rem;
+  width: 63rem;
+  column-gap: 5rem;
+  row-gap: 4rem;
+  overflow: scroll;
+`;
+
 function ResultButton() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
     <Wrapper>
       <Link href="/">
@@ -99,9 +181,10 @@ function ResultButton() {
           <ArrowImg src="/image/white_arrow_reverse.png" alt="화살표" />
         </PrevButtonText>
       </Link>
-      <ReplacementPlayerButton>
+      <ReplacementPlayerButton onClick={openModal}>
         <ButtonText>replacement player</ButtonText>
       </ReplacementPlayerButton>
+      {showModal ? <ModalFrame closeModal={closeModal} /> : null}
       <Link href="/">
         <ViewAllBox>
           <NextButtonText>view all</NextButtonText>
@@ -109,6 +192,26 @@ function ResultButton() {
         </ViewAllBox>
       </Link>
     </Wrapper>
+  );
+}
+
+function ModalFrame(closeModal) {
+  return (
+    <Container>
+      <Background>
+        <CloseButton src="/image/modal_close.png" onClick={closeModal} />
+        <ModalBlock>
+          <Contents>
+            <Title>replacement player</Title>
+            <Grid>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((i) => {
+                return <PlayerCard key={i} />;
+              })}
+            </Grid>
+          </Contents>
+        </ModalBlock>
+      </Background>
+    </Container>
   );
 }
 
---
+++ src/components/modal/ModalFrame.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: black;
+`;
+
+function ModalFrame() {
+  return <Container></Container>;
+}
+
+export default ModalFrame;
