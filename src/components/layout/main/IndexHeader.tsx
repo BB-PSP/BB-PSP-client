6dacf87a4c21aa373e7fa3a6e973ed24bfa2fb8b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-18 23:43:25 +0900

    style: :lipstick: 홈페이지 헤더 수정

--- src/components/layout/main/IndexHeader.tsx
+++ src/components/layout/main/IndexHeader.tsx
@@ -3,34 +3,24 @@ import React from 'react';
 import styled from 'styled-components';
 import { breakpoints } from '../../../styles/media';
 
-const Wrapper = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  ${breakpoints.large} {
-    top: 9.4rem;
-  }
-  ${breakpoints.medium} {
-    top: 9.4rem;
-  }
+const Wrapper = styled.nav`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 23.8rem;
+  padding-top: 9.4rem;
   ${breakpoints.small} {
-    top: 4rem;
   }
 `;
 
 const Title = styled.h1`
   cursor: grab;
   font-family: 'PlayfairDisplayBold';
-  ${breakpoints.large} {
-    font-size: 4.8rem;
-  }
-  ${breakpoints.medium} {
-    font-size: 4.8rem;
-  }
+  color: #b70000;
+  font-size: 4.8rem;
   ${breakpoints.small} {
     font-size: 4rem;
   }
-  color: #b70000;
 `;
 
 export default function IndexHeader() {
