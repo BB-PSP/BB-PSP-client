57eaa6b2972ce8f42073f19d8ea7fc0aff017388
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-20 13:03:44 +0900

    style: :lipstick: CommonLayout 반응형 수정

--- src/components/layout/common/CommonLayout.tsx
+++ src/components/layout/common/CommonLayout.tsx
@@ -5,48 +5,22 @@ import CommonHeader from './CommonHeader';
 
 const Container = styled.div`
   position: relative;
-  background-color: #ccc;
-  z-index: 1;
-  height: 100vh;
-  ${breakpoints.large} {
-    min-height: 108rem;
-  }
-  ${breakpoints.medium} {
-    min-height: 88rem;
-  }
-  ${breakpoints.small} {
-    min-height: 100vh;
-    @supports (-webkit-touch-callout: none) and (stroke-color: transparent) {
-      min-height: -webkit-fill-available;
-      position: fixed;
-    }
-  }
-`;
-
-const BackImage = styled.img`
-  position: absolute;
-  left: 50%;
-  transform: translate3d(-50%, 0, 0);
-  z-index: -99;
-  bottom: 0;
-  ${breakpoints.large} {
-    width: 104.6rem;
-    height: auto;
-  }
-  ${breakpoints.medium} {
-    width: 58rem;
-    height: auto;
-  }
-  ${breakpoints.small} {
-    width: 20rem;
-    height: auto;
-  }
-`;
-
-const Wrapper = styled.div`
-  position: absolute;
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  background-image: url('/image/baseball/baseball_gradationmask.png');
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  ${breakpoints.large} {
+    min-height: 76rem;
+    background-size: 54.5vw auto;
+  }
+  ${breakpoints.medium} {
+    background-size: 60vh auto;
+  }
+  ${breakpoints.small} {
+    height: 100vh;
+    background-size: 40vh auto;
+  }
 `;
 
 export default function CommonLayout({
@@ -56,14 +30,8 @@ export default function CommonLayout({
 }) {
   return (
     <Container>
-      <Wrapper>
-        <CommonHeader />
-        <BackImage
-          src="/image/baseball/baseball_gradationmask.png"
-          alt="home"
-        />
-        {children}
-      </Wrapper>
+      <CommonHeader />
+      {children}
     </Container>
   );
 }
