4646343e10bb782afe07af697cd8e2bb052af232
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-20 13:02:50 +0900

    style: :lipstick: useMediaQuery 분기점 수정

--- src/styles/MediaQuery.tsx
+++ src/styles/MediaQuery.tsx
@@ -3,21 +3,21 @@ import { useMediaQuery } from 'react-responsive';
 
 const Small: React.FC = ({ children }) => {
   const isMobile = useMediaQuery({
-    query: '(max-width:639px)',
+    query: '(max-width:767px)',
   });
   return <React.Fragment>{isMobile && children}</React.Fragment>;
 };
 
 const Medium: React.FC = ({ children }) => {
   const isTablet = useMediaQuery({
-    query: '(max-width: 1279px) and (min-width: 640px)',
+    query: '(max-width: 1024px) and (min-width: 768px)',
   });
   return <React.Fragment>{isTablet && children}</React.Fragment>;
 };
 
 const Large: React.FC = ({ children }) => {
   const isPc = useMediaQuery({
-    query: '(min-width:1280px) ',
+    query: '(min-width:1025px) ',
   });
   return <React.Fragment>{isPc && children}</React.Fragment>;
 };
