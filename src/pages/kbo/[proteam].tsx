077f5943bafc85ed5130330a5858addbb75858fb
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-31 18:13:46 +0900

    feat: 쿼리로 선택된 팀명 가져옴

--- src/pages/kbo/[proteam].tsx
+++ src/pages/kbo/[proteam].tsx
@@ -1,39 +1,26 @@
 import styled from 'styled-components';
 import CommonLayout from '../../components/layout/common/CommonLayout';
 import { breakpoints } from '../../styles/media';
-import { useRouter } from 'next/router';
 import PlayerCard from '../../components/PlayerCard/PlayerCard';
+import { useRouter } from 'next/router';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  ${breakpoints.large} {
-    width: 128rem;
-    padding-top: 39.6rem;
-  }
-  ${breakpoints.medium} {
-    width: 68rem;
-    padding-top: 24.6rem;
-  }
-  ${breakpoints.small} {
-    width: 35rem;
-    padding-top: 12.8rem;
-  }
 `;
 
 const Container = styled.div`
-  margin: 0 auto;
+  position: absolute;
+  bottom: 0;
+  margin-top: 19.54vh;
   ${breakpoints.large} {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    column-gap: 4rem;
-    row-gap: 7rem;
-    width: 135.5rem;
-    height: 62.6rem;
-    padding-right: 7.3rem;
+    column-gap: 2.08vw;
+    row-gap: 3.7vh;
+    width: 70.94vw;
+    height: 63.33vh;
     overflow-x: hidden;
     &::-webkit-scrollbar {
       width: 0.2rem;
@@ -60,24 +47,35 @@ const Container = styled.div`
       background-color: black;
     }
   }
-  ${breakpoints.small} {
-    display: flex;
-    flex-direction: column;
-    height: 45rem;
-    overflow-x: hidden;
-    overflow-y: scroll;
-  }
 `;
 
 function Team() {
   const router = useRouter();
-  console.log(router.query);
-
+  const team = router.query.proteam;
+  console.log(team);
+  // 쿼리로 선택한 팀을 받아온다.
+  // 그 팀의 선수들을 전부 불러오는 api를 호출한다.
   return (
     <Wrapper>
       <Container>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((i) => {
-          return <PlayerCard key={i} />;
+          return (
+            <PlayerCard
+              key={i}
+              age={0}
+              backNumber={0}
+              batInfo={''}
+              pitchInfo={''}
+              birth={''}
+              height={''}
+              weight={''}
+              highSchool={''}
+              position={''}
+              salary={''}
+              name={''}
+              team={''}
+            />
+          );
         })}
       </Container>
     </Wrapper>
