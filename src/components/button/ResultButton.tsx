e657a00c2423d5fd0f644e31234af648ca059c36
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-23 23:08:51 +0900

    refactor: 웹접근성에 맞게 버튼 수정

--- src/components/button/ResultButton.tsx
+++ src/components/button/ResultButton.tsx
@@ -8,7 +8,9 @@ const Wrapper = styled.div`
   display: flex;
   flex: 1;
   flex-direction: row;
+  justify-content: space-between;
   ${breakpoints.large} {
+    width: 61vw;
     height: 6.57vh;
     bottom: 0;
     margin-top: 6.48vh;
@@ -24,12 +26,11 @@ const Wrapper = styled.div`
   }
 `;
 
-const NextButtonText = styled.h3`
+const Button = styled.button`
   cursor: grab;
   font-family: 'RobotoMonoRegular';
   color: #fff;
   ${breakpoints.large} {
-    width: 2.76vw;
     font-size: 1.15vw;
     line-height: 2.69vh;
   }
@@ -69,26 +70,6 @@ const ViewAllBox = styled.div`
   justify-content: space-between;
 `;
 
-const PrevButtonText = styled.h3`
-  cursor: grab;
-  font-family: 'RobotoMonoRegular';
-  color: #fff;
-  text-align: right;
-  ${breakpoints.large} {
-    font-size: 1.15vw;
-    line-height: 2.69vh;
-    padding-right: 20.23vw;
-  }
-  ${breakpoints.medium} {
-    font-size: 2vw;
-    line-height: 3vh;
-  }
-  ${breakpoints.small} {
-    font-size: 2vw;
-    line-height: 3vh;
-  }
-`;
-
 const TestImage = styled.img`
   ${breakpoints.large} {
     margin-left: 0.52vw;
@@ -104,53 +85,43 @@ const TestImage = styled.img`
   }
   ${breakpoints.small} {
     margin-left: 0.52vw;
-    /* margin-top: 1.85vh; */
     width: 10vw;
     height: auto;
   }
 `;
 
-const ReplacementPlayerButton = styled.div`
+const ReplacementPlayerButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
   background: rgba(255, 255, 255, 0.74);
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
-  ${breakpoints.large} {
-    margin-top: 1.39vh;
-    padding: 1.11vh 1.46vw;
-    height: 5.19vh;
-    margin-right: 16.98vw;
-  }
-  ${breakpoints.medium} {
-    margin-top: 1.39vh;
-    width: 25vw;
-    height: 5.19vh;
-  }
-  ${breakpoints.small} {
-    margin-top: 1.39vh;
-    width: 25vw;
-    height: 5.19vh;
-    margin-left: 2vw;
-  }
-`;
-
-const ButtonText = styled.h3`
   font-family: 'RobotoMonoRegular';
   text-align: center;
   color: #272727;
   ${breakpoints.large} {
     font-size: 1.15vw;
     line-height: 2.69vh;
+    margin-top: 1.39vh;
+    padding: 1.11vh 1.46vw;
+    height: 5.19vh;
+    margin-left: 3vw;
   }
   ${breakpoints.medium} {
     font-size: 2vw;
     line-height: 3vh;
+    margin-top: 1.39vh;
+    width: 25vw;
+    height: 5.19vh;
   }
   ${breakpoints.small} {
     font-size: 2vw;
     line-height: 3vh;
+    margin-top: 1.39vh;
+    width: 25vw;
+    height: 5.19vh;
+    margin-left: 2vw;
   }
 `;
 
@@ -162,20 +133,28 @@ const ResultButton = () => {
   return (
     <Wrapper>
       <Link href="/">
-        <PrevButtonText>
+        <Button style={{ textAlign: 'right' }}>
           prev
-          <ArrowImg src="/image/white_arrow_reverse.png" alt="화살표" />
-        </PrevButtonText>
+          <ArrowImg
+            src="/image/white_arrow_reverse.png"
+            alt="이전 페이지 버튼"
+          />
+        </Button>
       </Link>
       <ReplacementPlayerButton onClick={openModal}>
-        <ButtonText>replacement player</ButtonText>
+        replacement player
       </ReplacementPlayerButton>
       {showModal ? <ModalFrame setShowModal={setShowModal} /> : null}
       <Link href="/custom/result/player/threeyear">
-        <ViewAllBox>
-          <NextButtonText>view all</NextButtonText>
-          <TestImage src="/image/white_arrow.png" />
-        </ViewAllBox>
+        <Button style={{ textAlign: 'left', width: '2.76vw' }}>
+          <ViewAllBox>
+            view all
+            <TestImage
+              src="/image/white_arrow.png"
+              alt="3년치 성적 예측 페이지 버튼"
+            />
+          </ViewAllBox>
+        </Button>
       </Link>
     </Wrapper>
   );
