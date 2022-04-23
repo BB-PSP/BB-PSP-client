4ae7fba78dbeee6ce311f7446d9264bc952683a8
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-23 22:44:26 +0900

    refactor: 웹접근성에 맞춰 버튼 수정

--- src/components/button/PageButton.tsx
+++ src/components/button/PageButton.tsx
@@ -3,48 +3,29 @@ import styled from 'styled-components';
 import { IButton } from '@store/Types';
 import { breakpoints } from '@styles/media';
 
-const ButtonContainer = styled.div`
+const ButtonContainer = styled.nav`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   position: absolute;
   ${breakpoints.large} {
-    bottom: 6.48vh;
+    width: 68vw;
+    bottom: 6.5vh;
   }
   ${breakpoints.medium} {
-    bottom: 2.87vh;
+    width: 80vw;
+    bottom: 2.9vh;
   }
   ${breakpoints.small} {
-    bottom: 2.87vh;
-  }
-`;
-
-const NextButtonText = styled.h3`
-  cursor: grab;
-  font-family: 'RobotoMonoRegular';
-  font-size: 2.2rem;
-  color: #b70000;
-  ${breakpoints.large} {
-    font-size: 1.15vw;
-    line-height: 2.69vh;
-    margin-left: 55.47vw;
-  }
-  ${breakpoints.medium} {
-    font-size: 2vw;
-    line-height: 3vh;
-    margin-left: 60vw;
-  }
-  ${breakpoints.small} {
-    font-size: 3.3vw;
-    line-height: 3vh;
-    margin-left: 60vw;
+    width: 90vw;
+    bottom: 1.8vh;
   }
 `;
 
 const ArrowImg = styled.img`
   ${breakpoints.large} {
     margin-top: -0.6rem;
-    width: 5.39vw;
+    width: 5.4vw;
     height: auto;
   }
   ${breakpoints.medium} {
@@ -56,25 +37,6 @@ const ArrowImg = styled.img`
     margin-top: -0.8vh;
     width: 13vw;
     height: auto;
-  }
-`;
-
-const PrevButtonText = styled.h3`
-  cursor: grab;
-  font-family: 'RobotoMonoRegular';
-  color: #b70000;
-  text-align: right;
-  ${breakpoints.large} {
-    font-size: 1.15vw;
-    line-height: 2.69vh;
-  }
-  ${breakpoints.medium} {
-    font-size: 2vw;
-    line-height: 3vh;
-  }
-  ${breakpoints.small} {
-    font-size: 3.3vw;
-    line-height: 3vh;
   }
 `;
 
@@ -82,7 +44,7 @@ const ReversedArrowImg = styled.img`
   transform: scaleX(-1);
   ${breakpoints.large} {
     margin-top: -0.6rem;
-    width: 5.39vw;
+    width: 5.4vw;
     height: auto;
   }
   ${breakpoints.medium} {
@@ -97,20 +59,38 @@ const ReversedArrowImg = styled.img`
   }
 `;
 
+const Button = styled.button`
+  cursor: grab;
+  font-family: 'RobotoMonoRegular';
+  color: #b70000;
+  ${breakpoints.large} {
+    font-size: 1.15vw;
+    line-height: 2.69vh;
+  }
+  ${breakpoints.medium} {
+    font-size: 2vw;
+    line-height: 3vh;
+  }
+  ${breakpoints.small} {
+    font-size: 3.3vw;
+    line-height: 3vh;
+  }
+`;
+
 function PageButton({ prev, next }: IButton) {
   return (
     <ButtonContainer>
       <Link href={prev}>
-        <PrevButtonText>
+        <Button style={{ textAlign: 'right' }}>
           prev
-          <ReversedArrowImg src="/image/arrow.png" alt="화살표" />
-        </PrevButtonText>
+          <ReversedArrowImg src="/image/arrow.png" alt="이전 페이지 버튼" />
+        </Button>
       </Link>
       <Link href={next}>
-        <NextButtonText>
+        <Button style={{ textAlign: 'left' }}>
           next
-          <ArrowImg src="/image/arrow.png" alt="화살표" />
-        </NextButtonText>
+          <ArrowImg src="/image/arrow.png" alt="다음 페이지 버튼" />
+        </Button>
       </Link>
     </ButtonContainer>
   );
