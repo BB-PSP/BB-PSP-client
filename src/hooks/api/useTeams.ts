f2f2a4a5aef6f22a419f85d20468e5b85de379e1
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-28 17:16:13 +0900

    백엔드 api 연동

--- src/hooks/api/useTeams.ts
+++ src/hooks/api/useTeams.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 const fetchTeams = async (year: number) => {
-  // const { data } = await axios('http://3.138.85.147:8081/api/teams/2021');
-  const { data } = await axios(`/api/team/${year}`);
+  const { data } = await axios(`http://18.223.133.67:8081/api/teams/${year}`);
+  // const { data } = await axios(`/api/team/${year}`);
   return data;
 };
 
