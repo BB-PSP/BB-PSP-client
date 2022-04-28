9622875dbe3eff0d68b5f00e2437b054a47f4c0f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-28 17:58:06 +0900

    next js에서 페이지는 style 파일 따로 분리하지 않음

--- src/pages/custom/position.tsx
+++ src/pages/custom/position.tsx
@@ -3,12 +3,59 @@ import position from '@data/position.json';
 import CommonLayout from '@layout/common/CommonLayout';
 import { selectedPositionState } from '@store/Data/atom';
 import { IPosition } from '@store/Types';
+import { breakpoints } from '@styles/media';
 import PositionCard from 'components/PositionCard';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
 
-import { GridContainer, Wrapper } from './styles';
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 10vh;
+`;
+
+export const GridContainer = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(6, 1fr);
+  ${breakpoints.large} {
+    padding-top: 5vh;
+    column-gap: 1vw;
+    row-gap: 6vh;
+    .column2 {
+      margin-left: 10vw;
+      grid-column: span 3;
+    }
+    .column3 {
+      margin-left: 3vw;
+      grid-column: span 2;
+    }
+  }
+  ${breakpoints.medium} {
+    padding-top: 8vh;
+    column-gap: 5vw;
+    row-gap: 3vh;
+    .column2 {
+      margin-left: 7vw;
+      grid-column: span 3;
+    }
+    .column3 {
+      grid-column: span 2;
+    }
+  }
+  ${breakpoints.small} {
+    height: 50vh;
+    row-gap: 5vh;
+    grid-template-columns: repeat(1, 1fr);
+    overflow-y: scroll;
+  }
+`;
 
 const imagePreload = (urls: string[]) => {
   urls.map((url) => {
