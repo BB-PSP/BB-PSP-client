b2aeb1357387323883d792f35dddd114b8418c2f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-12 15:58:39 +0900

    useQuery 옵션 추가

--- src/hooks/api/useTeams.ts
+++ src/hooks/api/useTeams.ts
@@ -3,12 +3,15 @@ import { useQuery } from 'react-query';
 
 const fetchTeams = async (year: number) => {
   const { data } = await axios(`http://18.223.133.67:8081/api/teams/${year}`);
-  // const { data } = await axios(`/api/team/${year}`);
   return data;
 };
 
 const useTeams = (year: number) => {
-  return useQuery('teamData', () => fetchTeams(year));
+  return useQuery('teamData', () => fetchTeams(year), {
+    keepPreviousData: true,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
 };
 
 export { useTeams, fetchTeams };
