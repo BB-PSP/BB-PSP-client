7d65a71b9016c2f1ea7fbc49058bd3a8e92b37a0
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-13 19:46:19 +0900

    feat: :sparkles: 투수, 타자를 고르는 컴포넌트 작성

---
+++ src/components/PositionSelectCard/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+
+import { PositionLogo, PositionName, Wrapper } from './styles';
+
+interface Props {
+  position: {
+    name: string;
+    colourLogo: string;
+    blackLogo: string;
+  };
+}
+
+const PositionSelectCard: FC<Props> = ({ position }) => {
+  return (
+    <Wrapper whileHover={{ scale: 1.05 }}>
+      <PositionLogo alt={position.name} src={position.blackLogo} />
+      <PositionName>{position.name}</PositionName>
+    </Wrapper>
+  );
+};
+
+export default PositionSelectCard;
