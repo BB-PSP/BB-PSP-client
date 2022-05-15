d212ff95ecfd5d8d7916c53e28b1a6e4734147b4
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-16 00:49:45 +0900

    데이터 페칭 훅 수정

--- src/hooks/api/usePlayers.ts
+++ src/hooks/api/usePlayers.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
-const fetchPlayers = async (year: number, symbol: string) => {
+const fetchPlayers = async (position: string, year: number, symbol: string) => {
   const { data } = await axios(
-    `http://18.223.133.67:8081/api/players/${year}/${symbol}`,
+    `http://18.223.133.67:8081/api/${position}/stat/${year}/${symbol}`,
   );
   return data;
 };
 
-const usePlayers = (year: number, symbol: string) => {
-  return useQuery('playerData', () => fetchPlayers(year, symbol), {
+const usePlayers = (position: string, year: number, symbol: string) => {
+  return useQuery('playerData', () => fetchPlayers(position, year, symbol), {
     keepPreviousData: true,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
