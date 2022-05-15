e28181e4a204e3a8ecb369ee28dd27f3b09443fc
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-16 00:50:06 +0900

    feat: 타입 수정

--- src/store/Types/index.ts
+++ src/store/Types/index.ts
@@ -4,6 +4,45 @@ export interface IPosition {
   blackLogo: string;
 }
 
+export interface IBatterProps {
+  batter_stat: {
+    _1B: number;
+    _2B: number;
+    _3B: number;
+    _AB: number;
+    _AVG: number;
+    _BB: number;
+    _CS: number;
+    _E: number;
+    _G: number;
+    _GDP: number;
+    _H: number;
+    _HBP: number;
+    _HR: number;
+    _IBB: number;
+    _OBP: number;
+    _PA: number;
+    _R: number;
+    _RBI: number;
+    _SB: number;
+    _SF: number;
+    _SH: number;
+    _SLG: number;
+    _SO: number;
+    _TB: number;
+  };
+  player_info: {
+    backNumber: string;
+    batInfo: string;
+    birth: string;
+    height: number;
+    weight: number;
+    name: string;
+    position: string;
+    team: string;
+  };
+}
+
 export interface IPlayer {
   age: number;
   backNumber: number;
