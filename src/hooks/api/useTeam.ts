f7c728f233cda31981fb1861f9f4a6f4cbcdff62
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-17 00:10:24 +0900

    단일팀 정보 페칭 훅

---
+++ src/hooks/api/useTeam.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const fetchTeam = async (year: number, team: string) => {
+  const { data } = await axios(
+    `http://18.223.133.67:8081/api/teams/${year}/${team}`,
+  );
+  return data;
+};
+
+const useTeam = (year: number, team: string) => {
+  return useQuery('team', () => fetchTeam(year, team), {
+    keepPreviousData: true,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export { useTeam, fetchTeam };
