b04ef7665d33fa6d41ad984fe83e5618947e4a0e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-17 16:14:16 +0900

    refactor: 라우팅 구조 변경

---
+++ src/pages/result/pitcher/[player]/[birth]/threeyear.tsx
@@ -0,0 +1,94 @@
+import PrevButton from '@button/PrevButton';
+import styled from '@emotion/styled';
+import BlackFullLayout from '@layout/black/BlackFullLayout';
+import StatTable from '@PlayerInfo/StatTable';
+import { breakpoints } from '@styles/media';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ContentsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(
+    180deg,
+    rgba(39, 39, 39, 0.87) 16.23%,
+    rgba(39, 39, 39, 0.433227) 65.57%,
+    rgba(39, 39, 39, 0) 100%
+  );
+  ${breakpoints.large} {
+    margin-top: 22.69vh;
+  }
+  ${breakpoints.medium} {
+    margin-top: 10vh;
+  }
+  ${breakpoints.small} {
+    margin-top: 5vh;
+  }
+`;
+
+const StatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  ${breakpoints.large} {
+    margin-top: 16.11vh;
+    margin-bottom: 6.48vh;
+  }
+  ${breakpoints.medium} {
+    margin-top: 16.11vh;
+    margin-bottom: 6.48vh;
+  }
+  ${breakpoints.small} {
+    margin-bottom: 3vh;
+  }
+`;
+
+const PlayerName = styled.h1`
+  position: absolute;
+  font-family: 'RobotoBoldItalic';
+  color: #fff;
+  ${breakpoints.large} {
+    font-size: 5.21vw;
+    margin-top: 14.07vh;
+  }
+  ${breakpoints.medium} {
+    font-size: 6vw;
+    margin-top: 6vh;
+  }
+  ${breakpoints.small} {
+    font-size: 6vw;
+    margin-top: 2vh;
+  }
+`;
+
+const Threeyear = () => {
+  return (
+    <Wrapper>
+      <PlayerName>Jung-hoo Lee #07</PlayerName>
+      <ContentsContainer>
+        <StatContainer>
+          <StatTable />
+          <StatTable />
+          <StatTable />
+        </StatContainer>
+        <ButtonContainer>
+          <PrevButton />
+        </ButtonContainer>
+      </ContentsContainer>
+    </Wrapper>
+  );
+};
+
+Threeyear.PageLayout = BlackFullLayout;
+
+export default Threeyear;
