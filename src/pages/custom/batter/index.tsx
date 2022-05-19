8e63d43498c7ea5b38949e3b0fe7606b51f08b28
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-19 23:48:54 +0900

    라우팅 구조 리팩토링

---
+++ src/pages/custom/batter/index.tsx
@@ -0,0 +1,86 @@
+import styled from '@emotion/styled';
+import { fetchPlayers, usePlayers } from '@hooks/api/usePlayers';
+import CommonLayout from '@layout/common/CommonLayout';
+import BatterCard from '@PlayerCard/BatterCard';
+import PitcherCard from '@PlayerCard/PitcherCard';
+import { IBatterProps, IPitcherProps } from '@store/Types';
+import { breakpoints } from '@styles/media';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { dehydrate, QueryClient } from 'react-query';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  ${breakpoints.medium} {
+    padding-top: 10vh;
+  }
+`;
+
+const Container = styled.div`
+  overflow-x: hidden;
+  ${breakpoints.large} {
+    position: absolute;
+    bottom: 0;
+    margin-top: 19.54vh;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 1.04vw;
+    row-gap: 3.7vh;
+    width: 70.94vw;
+    height: 63.33vh;
+    white-space: nowrap;
+    padding-left: 2vw;
+    padding-right: 2.6vw;
+  }
+  ${breakpoints.medium} {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 85vw;
+    height: 60vh;
+  }
+  &::-webkit-scrollbar {
+    background-color: transparent;
+    width: 0.4rem;
+  }
+  &::-webkit-scrollbar-track {
+    background: #fff;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #3d3d3d;
+    background-clip: padding-box;
+    &:hover {
+      background: #606060;
+    }
+  }
+`;
+
+const Batter = () => {
+  return (
+    <Wrapper>
+      <Container>pitcher</Container>
+    </Wrapper>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const proteam = context.params?.proteam as string;
+  const position = context.params?.position as string;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery('playerData', () =>
+    fetchPlayers(position, 2021, proteam),
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+Batter.PageLayout = CommonLayout;
+
+export default Batter;
---
+++ src/pages/custom/pitcher/index.tsx
@@ -0,0 +1,89 @@
+import styled from '@emotion/styled';
+import { useCustomPitcher } from '@hooks/api/useCustomPitcher';
+import { fetchPlayers, usePlayers } from '@hooks/api/usePlayers';
+import CommonLayout from '@layout/common/CommonLayout';
+import BatterCard from '@PlayerCard/BatterCard';
+import PitcherCard from '@PlayerCard/PitcherCard';
+import {
+  ageRangeState,
+  selectedPositionState,
+  selectedTeamState,
+} from '@store/Data/atom';
+import { IBatterProps, IPitcherProps } from '@store/Types';
+import { breakpoints } from '@styles/media';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { dehydrate, QueryClient } from 'react-query';
+import { useRecoilValue } from 'recoil';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  ${breakpoints.medium} {
+    padding-top: 10vh;
+  }
+`;
+
+const Container = styled.div`
+  overflow-x: hidden;
+  ${breakpoints.large} {
+    position: absolute;
+    bottom: 0;
+    margin-top: 19.54vh;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 1.04vw;
+    row-gap: 3.7vh;
+    width: 70.94vw;
+    height: 63.33vh;
+    white-space: nowrap;
+    padding-left: 2vw;
+    padding-right: 2.6vw;
+  }
+  ${breakpoints.medium} {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 85vw;
+    height: 60vh;
+  }
+  &::-webkit-scrollbar {
+    background-color: transparent;
+    width: 0.4rem;
+  }
+  &::-webkit-scrollbar-track {
+    background: #fff;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #3d3d3d;
+    background-clip: padding-box;
+    &:hover {
+      background: #606060;
+    }
+  }
+`;
+
+const Pitcher = () => {
+  const team = useRecoilValue(selectedTeamState);
+  const position = useRecoilValue(selectedPositionState);
+  const age = useRecoilValue(ageRangeState);
+  console.log(team, position, age);
+  const { data } = useCustomPitcher(2021, age[1], age[0], position, team);
+  console.log(data);
+  return (
+    <Wrapper>
+      <Container>pitcher</Container>
+    </Wrapper>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+Pitcher.PageLayout = CommonLayout;
+
+export default Pitcher;
