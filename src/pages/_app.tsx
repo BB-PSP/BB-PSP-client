d6491e9aaca8b9452d2078fab74d7ef12bd23671
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-23 15:06:19 +0900

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ function App({ Component, pageProps }: ComponentWithPageLayout) {
           <meta charSet="UTF-8" />
           <meta
             name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+            content="wi₩dth=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
           />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <title>BB-PSP</title>
