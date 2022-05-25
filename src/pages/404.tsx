8577cf82bbeacf2c0131360d600ac400978ddb2a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-25 14:55:54 +0900

    feat: 404 페이지

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,22 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > h1 {
+    font-family: 'RobotoMonoRegular';
+    font-size: 3vw;
+  }
+`;
+
 export default function NotFound() {
-  return <div>404</div>;
+  return (
+    <Wrapper>
+      <h1>404 not found</h1>
+    </Wrapper>
+  );
 }
