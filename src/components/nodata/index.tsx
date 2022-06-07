56c5fe14c408aee1e74699076ae6e8307455fc2d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-07 23:04:52 +0900

    feat: :sparkles: NoData 컴포넌트 생성

---
+++ src/components/nodata/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+const NoData = () => {
+  return (
+    <Container>
+      <h1>There is no Data ㅜ.ㅜ</h1>
+    </Container>
+  );
+};
+export default NoData;
---
+++ src/components/nodata/styles.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 30%;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  > h1 {
+    font-family: 'RobotoMonoRegular';
+    font-size: 3vw;
+  }
+`;
