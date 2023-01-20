35f59e5ce047904e19cfd7516714abeba247833c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-21 00:36:45 +0900

    feat: Home 페이지 진행

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
 import classNames from 'classnames/bind';
 
+import RightArrow from '@/assets/svg/icons/right_arrow.svg';
+import Background from '@/shared/Layout/Background';
+
 import styles from './index.module.scss';
 
 const cx = classNames.bind(styles);
 
 export default function Home() {
-  return <main className={cx('main')}>Home</main>;
+  return (
+    <main className={cx('main')}>
+      <Background />
+      <h1 className={cx('title')}>bb:</h1>
+      <h2 className={cx('sub_title')}>
+        BB:PSP(Baseball: Player Stats Prediction)
+      </h2>
+      <nav className={cx('nav')}>
+        <button className={cx('button')}>
+          Professional team
+          <RightArrow />
+        </button>
+        <button className={cx('button')}>
+          Choose it yourself
+          <RightArrow />
+        </button>
+      </nav>
+    </main>
+  );
 }
